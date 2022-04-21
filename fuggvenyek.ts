1ccad1fae256f7c7607f30c279279e094b9cba8f
FodorTamas <tamasfodor1988@gmail.com>
2022-04-21 18:14:28 +0200

    typescript függvények

--- fuggvenyek.ts
+++ fuggvenyek.ts
@@ -13,13 +13,6 @@ function NegyzetKeruletTeruletEljaras(a:number):void{
     document.write(`A(z) ${a} oldalú négyzet területe:${terulet}<br>`);
 }
 
-function TeglalapKeruletTeruletEljaras(a:number, b:number):void{
-    let kerulet=(a+b)*2;
-    let terulet=a*b;
-    document.write(`<br>A(z) ${a} és ${b} oldalú téglalap kerülete:${kerulet}<br>`);
-    document.write(`A(z) ${a} és ${b} oldalú téglalap területe:${terulet}<br>`);
-}
-
 //Visszatérési értékes és paraméter nélküli függvény
 function RandomGeneralo100Fuggveny():number{
     return Math.round(Math.random()*100);
---
+++ fuggvenyesFeladatok.ts
@@ -0,0 +1,6 @@
+function TeglalapKeruletTeruletEljaras(a:number, b:number):void{
+    let kerulet=(a+b)*2;
+    let terulet=a*b;
+    document.write(`<br>A(z) ${a} és ${b} oldalú téglalap kerülete:${kerulet}<br>`);
+    document.write(`A(z) ${a} és ${b} oldalú téglalap területe:${terulet}<br>`);
+}
