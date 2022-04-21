52f83b1beded2a586ecad37cf29afc38cafc1816
FodorTamas <tamasfodor1988@gmail.com>
2022-04-21 18:14:52 +0200

    typescript függvények

---
+++ fuggvenyesFeladatok.js
@@ -0,0 +1,6 @@
+function TeglalapKeruletTeruletEljaras(a, b) {
+    var kerulet = (a + b) * 2;
+    var terulet = a * b;
+    document.write("<br>A(z) ".concat(a, " \u00E9s ").concat(b, " oldal\u00FA t\u00E9glalap ker\u00FClete:").concat(kerulet, "<br>"));
+    document.write("A(z) ".concat(a, " \u00E9s ").concat(b, " oldal\u00FA t\u00E9glalap ter\u00FClete:").concat(terulet, "<br>"));
+}
